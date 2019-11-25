56ad0fed39666e20f68ba2a1af3fffdf9c90059f
Ophir Katz <ophir8396@campus.technion.ac.il>
2019-11-25 20:26:55 +0200

    added some poc's for certain custom components

--- BlazorPOC/Startup.cs
+++ BlazorPOC/Startup.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +23,7 @@ namespace BlazorPOC
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSignalR();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
