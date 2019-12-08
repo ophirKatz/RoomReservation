d8e05ecb48bc034f10c61552b70bc264f7b86f5d
Ophir Katz <ophir8396@campus.technion.ac.il>
2019-12-08 23:10:09 +0200

    working on room menu item

--- RoomResClient/Startup.cs
+++ RoomResClient/Startup.cs
@@ -47,6 +47,7 @@ namespace RoomResClient
             services.AddServerSideBlazor();
             services.AddSignalR();
             services.AddBlazoredToast();
+            services.AddBlazorContextMenu();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
